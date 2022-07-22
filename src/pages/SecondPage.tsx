457e6200511fd5d922e140e7e4ab76e85758cab0
ppasta90 <ppastacaldi@outlook.it>
2022-07-22 22:21:34 +0200

    add tooltip to copy button

--- src/pages/SecondPage.tsx
+++ src/pages/SecondPage.tsx
@@ -1,21 +1,22 @@
 import { useEffect, useRef, useState } from "react";
-import { Flex, Text, Box, Button, VStack } from "@chakra-ui/react";
+import { Flex, Text, Box, Button, VStack, Tooltip } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import { BsFillCheckCircleFill } from "react-icons/bs";
-
 // @ts-ignore
 import { Image, Transformation } from "cloudinary-react";
 function SecondPage() {
   const { publicId } = useParams();
   const [imageId, setImageId] = useState<string>("");
   const [copiedLink, setCopiedLink] = useState<string | null>("");
+  const [copied, setCopied] = useState(false);
   const linkRef = useRef<HTMLDivElement>(null);
 
   function handleCopyLink() {
     linkRef?.current?.firstChild &&
       setCopiedLink(linkRef?.current?.firstChild.textContent);
-    alert("copied");
+    setCopied(true);
   }
+
   useEffect(() => {
     navigator.clipboard.writeText(copiedLink || "");
   }, [copiedLink]);
@@ -35,11 +36,7 @@ function SecondPage() {
       height={"70vh"}
       background="white"
     >
-      <VStack
-        justifyContent="space-between"
-        /* border="1px solid red" */
-        height={"20%"}
-      >
+      <VStack justifyContent="space-between" height={"20%"}>
         <BsFillCheckCircleFill size={"4rem"} color="#219653" />
         <Text fontSize="2rem" color="#4F4F4F">
           Uploading successfull!
@@ -55,7 +52,7 @@ function SecondPage() {
         }}
       >
         <Image responsive cloudName="pasta" publicId={imageId} loading="lazy">
-          <Transformation radius="12" crop="fill" width="350" height="250" />
+          <Transformation radius="12" width="350" height="250" />
           <Transformation quality="auto" fetchFormat="auto" />
         </Image>
       </Box>
@@ -73,8 +70,12 @@ function SecondPage() {
         width="100%"
       >
         <Box textOverflow="ellipsis">{`http://res.cloudinary.com/pasta/image/upload/v1641508935/${publicId}`}</Box>
-        <Box>
-          <Button onClick={handleCopyLink}>Copy link</Button>
+        <Box onBlur={() => setCopied(false)}>
+          <Tooltip label={copied ? "Copied!" : "Copy"} closeOnClick={false}>
+            <Button color="white" bg={"#2F80ED"} onClick={handleCopyLink}>
+              Copy link
+            </Button>
+          </Tooltip>
         </Box>
       </Flex>
     </Flex>
