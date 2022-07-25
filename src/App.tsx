3cd3fe0e99b997e73c7873654f6d8dab2c4e6f72
ppasta90 <ppastacaldi@outlook.it>
2022-07-25 17:51:13 +0200

    add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Box, Flex, Link } from "@chakra-ui/react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import FirstPage from "./pages/FirstPage";
 import SecondPage from "./pages/SecondPage";
@@ -5,10 +6,29 @@ import SecondPage from "./pages/SecondPage";
 export default function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<FirstPage />} />
-        <Route path=":publicId" element={<SecondPage />} />
-      </Routes>
+      <Flex
+        direction="column"
+        justify="center"
+        align="center"
+        width="100%"
+        height="100%"
+      >
+        <Routes>
+          <Route path="/" element={<FirstPage />} />
+          <Route path=":publicId" element={<SecondPage />} />
+        </Routes>
+        <Box pos={"fixed"} bottom="10">
+          created by{" "}
+          <Link href="https://github.com/ppasta90" target="_blank">
+            {" "}
+            Paolo Pastacaldi{" "}
+          </Link>{" "}
+          -{" "}
+          <Link href="https://devchallenges.io/" target="_blank">
+            devChallenges.io
+          </Link>
+        </Box>
+      </Flex>
     </Router>
   );
 }
